235b243b6a1f4ef240c810adace4d7a74b54c7a0
Isabel Matos <ipmatos2000@gmail.com>
2023-03-15 15:27:38 -0300

    fixing url error

--- src/main_script.py
+++ src/main_script.py
@@ -11,11 +11,9 @@ token = os.environ["token"]
 url = os.environ["github_api_url"]
 repositories = []
 
-
-
-query = """
+query = '''
 query {
-  search(query: "language:java", type: REPOSITORY, first: 100, after: null) {
+  search(query: "stars:>100, language: Java", type: REPOSITORY, first: 20, after: null) {
     nodes {
       ... on Repository {
         nameWithOwner
@@ -33,25 +31,20 @@ query {
     }
   }
 }
-"""
-
+'''
 
 headers = {"Authorization": "Bearer " + token}
 count = 0
 data = []
-cursor = None 
-
+cursor = None
 response = requests.post(url, json={"query": query}, headers=headers)
 
-breakpoint()
 while count < 1000:
-
     if cursor:
-        query_with_cursor = query.replace('after: null', 'after: "%s"' % cursor)
-
-        response = requests.post(url, json={"query": query_with_cursor}, headers=headers)
-    else:
-        response = requests.post(url, json={"query": query}, headers=headers)
+        query_with_cursor = query.replace(
+            'after: null', 'after: "%s"' % cursor)
+        response = requests.post(
+            url, json={"query": query_with_cursor}, headers=headers)
 
     if response.status_code == 200:
         repos = response.json()['data']['search']['nodes']
@@ -62,17 +55,20 @@ while count < 1000:
             count += 1
             name = repo['nameWithOwner']
             stars = repo['stargazerCount']
-            created_at = datetime.strptime(repo['createdAt'], '%Y-%m-%dT%H:%M:%SZ')
+            created_at = datetime.strptime(
+                repo['createdAt'], '%Y-%m-%dT%H:%M:%SZ')
             age = round((datetime.utcnow() - created_at).days / 365.25, 2)
-            url = repo['url']
-            row = [count, name, stars, age, url]
+            num_releases = repo['releases']['totalCount']
+            repo_url = repo['url']
+            row = [count, name, stars, age, num_releases, repo_url]
             data.append(row)
+        print(count)
     else:
-         continue
+        continue
 
 with open('arquivo1.csv', 'w', newline='') as file:
     writer = csv.writer(file)
-    writer.writerow(['Número', 'Nome', 'Estrelas', 'Idade (anos)', 'Url'])
+    writer.writerow(['Número', 'Nome', 'Estrelas',
+                    'Idade (anos)', 'Número de releases', 'Url'])
     for row in data:
         writer.writerow(row)
-
